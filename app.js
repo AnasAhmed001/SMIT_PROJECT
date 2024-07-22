5a975c3cf47379bc2a164c96f7d0e5ff52530760
AnasAhmed001 <hafizanasahmed8@gmail.com>
2024-07-22 10:47:41 +0500

    project changed

--- app.js
+++ app.js
@@ -1,2 +1,6 @@
-const desciption = document.querySelector = "#description";
+const description = document.querySelector = "#description";
 const amount = document.querySelector = "#amount";
+const totalAmount = document.querySelector ="total-expense";
+const button = document.querySelector = "#button";
+const ul = document.querySelector = "ul";
+
